a876d8754a098ab94fad3a79fcda2d6183912412
MikeKa <mkashirsky@gmail.com>
2016-02-24 21:15:33 +0300

    Set up project

---
+++ config/environments/staging.rb
@@ -0,0 +1,13 @@
+# Staging configuration is identical to production, with some overrides
+# for hostname, etc.
+
+require_relative "./production"
+
+Rails.application.configure do
+
+  config.action_mailer.default_url_options = {
+    :host => "localhost",
+    :protocol => "https"
+  }
+  config.action_mailer.asset_host = "https://localhost"
+end
